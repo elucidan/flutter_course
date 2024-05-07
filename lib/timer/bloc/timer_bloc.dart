fc0b63607ee891833089223c89533401ba766236
Davey Boyle <davey@elucidan.com>
2024-05-07 21:16:43 +0100

    Did a block exercise

--- lib/timer/bloc/timer_bloc.dart
+++ lib/timer/bloc/timer_bloc.dart
@@ -17,12 +17,51 @@ class TimerBloc extends Bloc<TimerEvent, TimerState> {
       : _ticker = ticker,
         super(const TimerInitial(_duration)) {
     on<TimerStarted>(_onStarted);
+    on<_TimerTicked>(
+        _onTicked); // I guess the underscore is because you don't send a command to tick its internal
+    on<TimerResumed>(_onResumed);
+    on<TimerPaused>(_onPaused);
+    on<TimerReset>(_onReset);
   }
+
+  @override
+  Future<void> close() {
+    _tickerSubscription?.cancel();
+    return super.close();
+  }
+
   void _onStarted(TimerStarted event, Emitter<TimerState> emit) {
     emit(TimerRunInProgress(event.duration));
     _tickerSubscription?.cancel();
     _tickerSubscription = _ticker
         .tick(ticks: event.duration)
         .listen((duration) => add(_TimerTicked(duration: duration)));
+  }
+
+  void _onPaused(TimerPaused event, Emitter<TimerState> emit) {
+    if (state is TimerRunInProgress) {
+      _tickerSubscription?.pause();
+      emit(TimerRunPause(state.duration));
+    }
+  }
+
+  void _onTicked(_TimerTicked event, Emitter<TimerState> emit) {
+    emit(
+      event.duration > 0
+          ? TimerRunInProgress(event.duration)
+          : const TimerRunComplete(),
+    );
+  }
+
+  void _onReset(TimerReset event, Emitter<TimerState> emit) {
+    _tickerSubscription?.cancel();
+    emit(const TimerInitial(_duration));
+  }
+
+  void _onResumed(TimerResumed event, Emitter<TimerState> emit) {
+    if (state is TimerRunPause) {
+      _tickerSubscription?.resume();
+      emit(TimerRunInProgress(state.duration));
+    }
   }
 }
